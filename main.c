a5099aeb53453c7d17aabd68d97cfab3e4c793b1
raffs <raffs.cardoso15@gmail.com>
2025-02-03 02:33:54 -0300

    minor final changes on the initial window setup

--- main.c
+++ main.c
@@ -9,14 +9,11 @@
 #define SCREEN_HEIGHT 480
 
 // How to compile: gcc main.c -lSDL2 -lSDL2main -o main.out
-int main(int argc, char *args[])
-{
+int main(int argc, char *args[]){
   SDL_Window* window = NULL;
-  SDL_Surface* surface = NULL; 
 
   if(SDL_Init(SDL_INIT_EVERYTHING) < 0){
     printf("SDL could not initialize,SDL_Error: %s\n",SDL_GetError());
-    
   } else {
     window = SDL_CreateWindow("tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT,SDL_WINDOW_SHOWN);
     if(window == NULL){
@@ -25,12 +22,12 @@ int main(int argc, char *args[])
       int keep_window_open = 1;
       while(keep_window_open){
          SDL_Event e;
+
          while(SDL_PollEvent(&e) > 0){
-          switch(e.type)
-        {
-            case SDL_QUIT:
-                keep_window_open = 0;
-                break;
+            switch(e.type){
+               case SDL_QUIT:
+               keep_window_open = 0;
+               break;
         }
 
         SDL_UpdateWindowSurface(window);
