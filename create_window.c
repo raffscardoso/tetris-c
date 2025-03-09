8530431a5961bd972958ee7925ae5f5a6cca5e5b
raffs <raffs.cardoso15@gmail.com>
2025-03-08 22:43:54 -0300

    changing the objects creation logic with structs

--- create_window.c
+++ create_window.c
@@ -6,12 +6,42 @@
 #include <SDL2/SDL_timer.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
+typedef struct {
+  float pos_x, pos_y, width, height;
 
-void create_rect(SDL_Renderer *renderer, int pos_x, int pos_y){
+  SDL_Color color;
+
+  int is_active;
+
+  float speed_x, speed_y;
+
+
+  
+} GameObject;
+
+GameObject* create_rect(float pos_x, float pos_y, float width, float height, SDL_Color color){
+  GameObject* obj = (GameObject*)malloc(sizeof(GameObject));
+  if(!obj) return NULL;
+  
+  obj->pos_x = pos_x;
+  obj->pos_y = pos_y;
+  obj->width = width;
+  obj->height = height;
+  obj->color = color;
+  obj->is_active = 1;
+  obj->speed_x = 0;
+  obj->speed_y = 0;
+
+
+  return obj;
+}
+
+/* void create_rect(SDL_Renderer *renderer, int pos_x, int pos_y){
   SDL_Rect rect;
   rect.x = pos_x;
   rect.y = pos_y;
@@ -22,13 +52,14 @@ void create_rect(SDL_Renderer *renderer, int pos_x, int pos_y){
   SDL_RenderDrawRect(renderer,&rect);
   SDL_RenderFillRect(renderer, &rect);
  
-}
+} */
 
 void create_window(SDL_Window *window, SDL_Renderer *renderer){
 
-  int pos_x = 270;
+  /* int pos_x = 270;
   float pos_y = 10.0f;
   int gravity_y = 2;
+  */
   
   if (SDL_Init(SDL_INIT_EVERYTHING)) {
       printf("SDL could not initialize,SDL_Error: %s\n", SDL_GetError());
@@ -58,16 +89,16 @@ void create_window(SDL_Window *window, SDL_Renderer *renderer){
             float delta_time = (current_time - last_time)/ 1000.0f;
             last_time = current_time;
 
-            pos_y += speed * delta_time;
+             //pos_y += speed * delta_time;
             
             
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
             SDL_RenderClear(renderer);
 
-            create_rect(renderer, pos_x, (int)pos_y);
+            //create_rect(renderer, pos_x, (int)pos_y);
 
             SDL_RenderPresent(renderer);
-
+            SDL_Delay(16);
           }
         }
       }
