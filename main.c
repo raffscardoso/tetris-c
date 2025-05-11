743b0b6c8d0058641a939508eb9d2d58ecc1ea27
raffs <raffs.cardoso15@gmail.com>
2025-05-11 20:36:26 -0300

    restarting the project

--- main.c
+++ main.c
@@ -1,22 +1,14 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_error.h>
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_timer.h>
-#include <SDL2/SDL_video.h>
 #include <stdio.h>
-#include "create_window.h"
+#include <stdlib.h>
+
+#define SCREEN_WIDTH  640
+#define SCREEN_HEIGHT 480
+
+int main(int argc, char *argv[])
+{
 
 
-// How to compile: gcc main.c create_window.c -lSDL2 -lSDL2main -o main.out
-int main(int argc, char *args[]) {
-  SDL_Window *window = NULL;
-  SDL_Renderer *renderer = NULL;
 
-  create_window(window,renderer);
-
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-  return 0;
+  return EXIT_SUCCESS;
 }
