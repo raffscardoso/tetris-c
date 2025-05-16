3fefa8b490ed05f15025675dc6b71501871b3718
raffs <raffs.cardoso15@gmail.com>
2025-05-16 14:29:02 -0300

    creating a renderer

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <SDL2/SDL_error.h>
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_render.h>
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_video.h>
 #include <stdio.h>
@@ -12,21 +13,28 @@
 #define SCREEN_HEIGHT 480
 
 int main(int argc, char *argv[])
-{
-  SDL_Window* window = NULL;
-  SDL_Surface* screenSurface = NULL;
+{ 
   if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
     printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
   }
 
-  window = SDL_CreateWindow("tetris", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED , SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-  if (window == NULL){
+
+  SDL_Window* window = SDL_CreateWindow("tetris", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED , SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (!window){
     printf("Window creation error! SDL_Error: %s\n", SDL_GetError() );
+    SDL_Quit();
+    return EXIT_FAILURE;
   }
 
-  screenSurface = SDL_GetWindowSurface(window);
-  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 0, 0) );
-  SDL_UpdateWindowSurface(window);
+
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+  if(!renderer){
+    printf("Renderer creation failed! SDL_Error: %s\n", SDL_GetError() );
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_FAILURE;
+  }
+
 
   SDL_Event e;
   bool quit = false;
@@ -38,5 +46,8 @@ int main(int argc, char *argv[])
     }
   }
 
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   return EXIT_SUCCESS;
 }
