e5457868001aaa3229398773e749307073744817
raffs <raffs.cardoso15@gmail.com>
2025-08-27 14:15:05 -0300

    reset do projeto

--- main.c
+++ main.c
@@ -1,69 +1,8 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_error.h>
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_pixels.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_surface.h>
-#include <SDL2/SDL_video.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include "create_object.h"
 
-#define SCREEN_WIDTH  640
-#define SCREEN_HEIGHT 480
-
-int main(int argc, char *argv[])
-{ 
-  if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-    printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    return EXIT_FAILURE;
-  }
+int main(int argc, char *argv[]) { 
 
 
-  SDL_Window* window = SDL_CreateWindow("tetris", SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED , SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-  if (!window){
-    printf("Window creation error! SDL_Error: %s\n", SDL_GetError() );
-    SDL_Quit();
-    return EXIT_FAILURE;
-  }
-
-
-  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  if(!renderer){
-    printf("Renderer creation failed! SDL_Error: %s\n", SDL_GetError() );
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return EXIT_FAILURE;
-  }
-
-  SDL_Event e;
-  bool quit = false;
-
-  int rectangle_width = 150;
-  int rectangle_height = 50;
-  int rectangle_x = (SCREEN_WIDTH - rectangle_width) / 2;
-  int rectangle_y = (SCREEN_HEIGHT - rectangle_height) / 2;
-
-
-
-
-  while (!quit) {
-    while (SDL_PollEvent(&e)) {
-      if (e.type == SDL_QUIT) {
-        quit = true;
-      }
-    }
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-
-    create_rectangle(renderer, rectangle_width, rectangle_height, rectangle_x, rectangle_y);
-
-    SDL_RenderPresent(renderer);
-  }
-
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-  return EXIT_SUCCESS;
+  return 0; 
 }
